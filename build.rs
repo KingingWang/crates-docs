5b5fc7ec66092842f9ec6bfef2e67f831cba928d
kingingwang <kingingwang@foxmail.com>
2026-02-13 08:17:41 +0800

    fmt

--- build.rs
+++ build.rs
@@ -12,17 +12,19 @@ fn main() {
     if let Ok(output) = Command::new("git")
         .args(["rev-parse", "--short", "HEAD"])
         .output()
-        && output.status.success() {
-            commit = String::from_utf8_lossy(&output.stdout).trim().to_string();
-        }
+        && output.status.success()
+    {
+        commit = String::from_utf8_lossy(&output.stdout).trim().to_string();
+    }
     println!("cargo:rustc-env=GIT_COMMIT={}", commit);
 
     // 获取 Rust 版本
     let mut version = String::from("unknown");
     if let Ok(output) = Command::new("rustc").args(["--version"]).output()
-        && output.status.success() {
-            version = String::from_utf8_lossy(&output.stdout).trim().to_string();
-        }
+        && output.status.success()
+    {
+        version = String::from_utf8_lossy(&output.stdout).trim().to_string();
+    }
     println!("cargo:rustc-env=RUST_VERSION={}", version);
 
     // 重新 cargo rerun 逻辑
